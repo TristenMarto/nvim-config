0a600cb2999c069665f15f2f2fac5869fbcf6e4c
TristenMarto <tristen.marto@gmail.com>
2025-01-18 22:10:41 +0100

    update telescope and install whichkey

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,30 +3,30 @@ return {
 		"williamboman/mason.nvim",
 		config = function()
 			require("mason").setup()
-		end
+		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = {"lua_ls", "pyright", "omnisharp", "csharp_ls"}
+				ensure_installed = { "lua_ls", "pyright", "omnisharp", "csharp_ls" },
 			})
-		end
+		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
-    lazy = false,
+		lazy = false,
 		config = function()
 			local lspconfig = require("lspconfig")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			lspconfig.lua_ls.setup({capabilities = capabilities})
-			lspconfig.pyright.setup({capabilities = capabilities})
-			lspconfig.csharp_ls.setup({capabilities = capabilities})
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.pyright.setup({ capabilities = capabilities })
+			lspconfig.csharp_ls.setup({ capabilities = capabilities })
 
-			vim.keymap.set('n','K', vim.lsp.buf.hover, {})
-			vim.keymap.set('n','K', vim.lsp.buf.hover, {})
-			vim.keymap.set({'n','v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-		end
-	}
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,12 @@
-return { 
-	"nvim-treesitter/nvim-treesitter", 
+return {
+	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = {"lua", "c_sharp", "python"},
-			highlight = {enabled = true},
+			ensure_installed = { "lua", "c_sharp", "python" },
+			highlight = { enabled = true },
 			indent = { enabled = true },
 		})
-	end
+	end,
 }
